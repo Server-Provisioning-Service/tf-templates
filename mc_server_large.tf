bd545b2bf432b4ff7b2046881ca22cef3a5cc747
Peter Strong <peterstrongsec@gmail.com>
2025-06-13 15:42:31 -0400

    Formatting

--- mc_server_large.tf
+++ mc_server_large.tf
@@ -1,8 +1,8 @@
 # Build for large Minecraft server
 
 resource "aws_instance" "MC_SERVER_LARGE" {
-  ami           = local.mc_server_ami
-  instance_type = local.mc_large_type
+  ami                    = local.mc_server_ami
+  instance_type          = local.mc_large_type
   vpc_security_group_ids = [aws_security_group.MC_SERVER_SG.id]
 
   tags = {
--- mc_server_medium.tf
+++ mc_server_medium.tf
@@ -1,8 +1,8 @@
 # Build for medium Minecraft server
 
 resource "aws_instance" "MC_SERVER_MEDIUM" {
-  ami           = local.mc_server_ami
-  instance_type = local.mc_medium_type
+  ami                    = local.mc_server_ami
+  instance_type          = local.mc_medium_type
   vpc_security_group_ids = [aws_security_group.MC_SERVER_SG.id]
 
   tags = {
