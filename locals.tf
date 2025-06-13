ee02c7be7771721143fc864eb8302449110c0bb9
Peter Strong <peterstrongsec@gmail.com>
2025-06-13 15:53:24 -0400

    Minor changes

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   region = "us-east-2"
 
   # AMIs
-  mc_server_ami = "ami-0d1b5a8c13042c939"       # Default Ubuntu LTS AMI provided by AWS
+  mc_server_ami = "ami-0d1b5a8c13042c939" # Default Ubuntu LTS AMI provided by AWS
 
   # Instance Types
   mc_small_type  = "t2.micro"
